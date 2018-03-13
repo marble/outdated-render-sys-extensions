e2a7f62344e07bb1d43f5a757a55e27fb2bebf3f
Martin Bless <martin.bless@mbless.de>
2018-03-13 10:23:07 +0100

    Save all

---
+++ do-the-work.sh
@@ -0,0 +1,254 @@
+# do-the-work-sh, mb, 2012-04-22, 2012-04-24
+
+EXTENSIONS=/home/mbless/public_html/typo3/extensions
+
+EXTKEY=css_styled_content
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE='f=typo3/sysext/'$EXTKEY'/doc/manual.sxw'
+
+EXTKEY=felogin
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=form
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/Documentation/Manual/en/manual.sxw"
+
+EXTKEY=openid
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=recycler
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=rsaauth
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=rtehtmlarea
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=saltedpasswords
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=scheduler
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=sys_action
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=taskcenter
+GITREPOS="https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;"
+GITFILE="f=typo3/sysext/"$EXTKEY"/doc/manual.sxw"
+
+EXTKEY=dbal
+GITREPOS="https://git.typo3.org/TYPO3CMS/Extensions/$EXTKEY.git?a=blob_plain;"
+GITFILE="f=doc/manual.sxw"
+
+EXTKEY=linkvalidator
+GITREPOS="https://git.typo3.org/TYPO3CMS/Extensions/$EXTKEY.git?a=blob_plain;"
+GITFILE="f=doc/manual.sxw"
+
+EXTKEY=workspaces
+GITREPOS="https://git.typo3.org/TYPO3CMS/CoreProjects/workspaces/workspaces.git?a=blob_plain;"
+GITFILE="f=Documentation/manual.sxw"
+
+GITURL=$GITREPOS$GITFILE
+
+
+
+EXTKEY=doc_core_tsref
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_tsref/trunk/doc_core_tsref/doc/manual.sxw"
+
+EXTKEY=doc_core_api
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_api/trunk/doc_core_api/doc/manual.sxw"
+
+EXTKEY=doc_core_cgl
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_cgl/trunk/doc_core_cgl/doc/manual.sxw"
+
+EXTKEY=doc_core_inside
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_inside/trunk/doc_core_inside/doc/doc_core_inside.sxw"
+
+EXTKEY=doc_core_services
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_services/trunk/doc_core_services/doc/doc_core_services.sxw"
+
+EXTKEY=doc_core_skinning
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_skinning/trunk/doc_core_skinning/doc/manual.sxw"
+
+EXTKEY=doc_core_tca
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_skinning/trunk/doc_core_skinning/doc/manual.sxw"
+
+EXTKEY=doc_core_ts
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_ts/trunk/doc_core_ts/doc/manual.sxw"
+
+EXTKEY=doc_core_tsconfig
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_core_tsconfig/trunk/doc_core_tsconfig/doc/manual.sxw"
+
+EXTKEY=doc_guide_admin
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_guide_admin/trunk/doc_guide_admin/doc/manual.sxw"
+
+EXTKEY=doc_guide_install
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_guide_install/trunk/doc_guide_install/doc/manual.sxw"
+
+EXTKEY=doc_guide_l10n
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_guide_l10n/trunk/doc_guide_l10n/doc/manual.sxw"
+
+EXTKEY=doc_guide_security
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_guide_security/trunk/doc_guide_security/doc/manual.sxw"
+
+EXTKEY=doc_indexed_search
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_indexed_search/trunk/doc/doc_indexed_search.sxw"
+
+EXTKEY=doc_template
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_template/trunk/doc/manual.sxw"
+
+EXTKEY=doc_tut_editors
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_tut_editors/trunk/doc_tut_editors/doc/manual.sxw"
+
+EXTKEY=doc_tut_quickstart
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_tut_quickstart/trunk/doc_tut_quickstart/doc/manual.sxw"
+
+EXTKEY=doc_tut_ts45
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/doc_tut_ts45/trunk/doc_tut_ts45/doc/manual.sxw"
+
+EXTKEY=official_template
+GITFILE=
+GITURL="https://svn.typo3.org/TYPO3v4/Documentation/official_template/trunk/OpenOffice/official_documentation_template.ott"
+
+#### https://svn.typo3.org/TYPO3v4/Documentation/doc_tut_templating/
+#### https://svn.typo3.org/TYPO3v4/Documentation/doc_tut_templavoila/trunk/
+
+
+EXTKEY=commerce
+GITFILE=
+GITURL="http://typo3.org/extension-manuals/commerce/current/sxw/?no_cache=1"
+GITURL="https://svn.typo3.org/TYPO3v4/Extensions/commerce/trunk/doc/manual.sxw"
+
+
+
+if [ ! -e $EXTENSIONS/$EXTKEY ]
+then
+    mkdir  $EXTENSIONS/$EXTKEY
+fi
+if [ ! -e $EXTENSIONS/$EXTKEY/nightly ]
+then
+    mkdir $EXTENSIONS/$EXTKEY/nightly
+fi
+rm -rf $EXTENSIONS/$EXTKEY/nightly/*
+
+# step: get the manual
+wget -nv $GITURL -O $EXTENSIONS/$EXTKEY/nightly/manual.sxw
+
+# step: let OpenOffice write the HTML version
+python odconverter.py   $EXTENSIONS/$EXTKEY/nightly/manual.sxw      $EXTENSIONS/$EXTKEY/nightly/manual.html
+
+# step: correct rights
+chmod -R +r             $EXTENSIONS/$EXTKEY/*
+
+# step: copy to temp file and thereby drop the <SDFIELD> tags. Otherwise Tidy won't work.
+python copyclean.py     $EXTENSIONS/$EXTKEY/nightly/manual.html      $EXTENSIONS/$EXTKEY/nightly/1-cleaned.html 
+
+# step: Use tidy to convert from HTML-4 to XHTML
+tidy -asxhtml -utf8 -f $EXTENSIONS/$EXTKEY/nightly/tidy-error-log.txt -o $EXTENSIONS/$EXTKEY/nightly/2-from-tidy.html $EXTENSIONS/$EXTKEY/nightly/1-cleaned.html
+
+# step: convert from XHTML to reST
+python ooxhtml2rst-work-in-progress.py  --treefile=$EXTENSIONS/$EXTKEY/nightly/restparser-tree.txt --logfile=$EXTENSIONS/$EXTKEY/nightly/restparser-log.txt  $EXTENSIONS/$EXTKEY/nightly/2-from-tidy.html $EXTENSIONS/$EXTKEY/nightly/manual.rst  1>&2 2>$EXTENSIONS/$EXTKEY/nightly/restparser-errors.txt
+
+# step: Generate single HTML file from single reST file using the Docutils (no Sphinx involved here)
+rst2htmltypo3.py --link-stylesheet --stylesheet-path=/css/typo3_docutils_styles.css --warnings=$EXTENSIONS/$EXTKEY/nightly/rst2html-warnings.txt   $EXTENSIONS/$EXTKEY/nightly/manual.rst $EXTENSIONS/$EXTKEY/nightly/index.html
+
+# Done so far.
+
+
+
+#
+#
+#if [ ! -e $EXTENSIONS/$EXTKEY/4.7 ]
+#then
+#    mkdir $EXTENSIONS/$EXTKEY/4.7
+#if
+#rm -rf $EXTENSIONS/$EXTKEY/4.7/*
+#wget "https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;hb=TYPO3_4-7;"$GITFILE  -O $EXTENSIONS/$EXTKEY/4.7/manual.sxw
+#python odconverter.py   $EXTENSIONS/$EXTKEY/4.7/manual.sxw      $EXTENSIONS/$EXTKEY/4.7/manual.html
+#chmod -R +r             $EXTENSIONS/$EXTKEY/*
+#python copyclean.py     $EXTENSIONS/$EXTKEY/4.7/manual.html      $EXTENSIONS/$EXTKEY/4.7/1-cleaned.html 
+#tidy -asxhtml -utf8 -f $EXTENSIONS/$EXTKEY/4.7/tidy-error-log.txt -o $EXTENSIONS/$EXTKEY/4.7/2-from-tidy.html $EXTENSIONS/$EXTKEY/4.7/1-cleaned.html
+#python ooxhtml2rst-work-in-progress.py   $EXTENSIONS/$EXTKEY/4.7/2-from-tidy.html $EXTENSIONS/$EXTKEY/4.7/manual.rst
+#rst2htmltypo3.py --link-stylesheet --stylesheet-path=/css/typo3_docutils_styles.css $EXTENSIONS/$EXTKEY/4.7/manual.rst $EXTENSIONS/$EXTKEY/4.7/index.html
+#
+#
+#
+#if [ ! -e $EXTENSIONS/$EXTKEY/4.6 ]
+#then
+#    mkdir $EXTENSIONS/$EXTKEY/4.6
+#if
+#rm -rf $EXTENSIONS/$EXTKEY/4.6/*
+#wget "https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;hb=TYPO3_4-6;"$GITFILE  -O $EXTENSIONS/$EXTKEY/4.6/manual.sxw
+#python odconverter.py   $EXTENSIONS/$EXTKEY/4.6/manual.sxw      $EXTENSIONS/$EXTKEY/4.6/manual.html
+#chmod -R +r             $EXTENSIONS/$EXTKEY/*
+#python copyclean.py     $EXTENSIONS/$EXTKEY/4.6/manual.html      $EXTENSIONS/$EXTKEY/4.6/1-cleaned.html 
+#tidy -asxhtml -utf8 -f $EXTENSIONS/$EXTKEY/4.6/tidy-error-log.txt -o $EXTENSIONS/$EXTKEY/4.6/2-from-tidy.html $EXTENSIONS/$EXTKEY/4.6/1-cleaned.html
+#python ooxhtml2rst-work-in-progress.py   $EXTENSIONS/$EXTKEY/4.6/2-from-tidy.html $EXTENSIONS/$EXTKEY/4.6/manual.rst
+#rst2htmltypo3.py --link-stylesheet --stylesheet-path=/css/typo3_docutils_styles.css $EXTENSIONS/$EXTKEY/4.6/manual.rst $EXTENSIONS/$EXTKEY/4.6/index.html
+#
+#
+#
+#if [ ! -e $EXTENSIONS/$EXTKEY/4.5 ]
+#then
+#    mkdir $EXTENSIONS/$EXTKEY/4.5
+#if
+#rm -rf $EXTENSIONS/$EXTKEY/4.5/*
+#wget "https://git.typo3.org/Packages/TYPO3.CMS.git?a=blob_plain;hb=TYPO3_4-5;"$GITFILE  -O $EXTENSIONS/$EXTKEY/4.5/manual.sxw
+#python odconverter.py   $EXTENSIONS/$EXTKEY/4.5/manual.sxw      $EXTENSIONS/$EXTKEY/4.5/manual.html
+#chmod -R +r             $EXTENSIONS/$EXTKEY/*
+#python copyclean.py     $EXTENSIONS/$EXTKEY/4.5/manual.html      $EXTENSIONS/$EXTKEY/4.5/1-cleaned.html 
+#tidy -asxhtml -utf8 -f $EXTENSIONS/$EXTKEY/4.5/tidy-error-log.txt -o $EXTENSIONS/$EXTKEY/4.5/2-from-tidy.html $EXTENSIONS/$EXTKEY/4.5/1-cleaned.html
+#python ooxhtml2rst-work-in-progress.py   $EXTENSIONS/$EXTKEY/4.5/2-from-tidy.html $EXTENSIONS/$EXTKEY/4.5/manual.rst
+#rst2htmltypo3.py --link-stylesheet --stylesheet-path=/css/typo3_docutils_styles.css $EXTENSIONS/$EXTKEY/4.5/manual.rst $EXTENSIONS/$EXTKEY/4.5/index.html
+#
+
+
+
+
+## FMY - for my interest
+## the following is the contents of: https://svn.typo3.org/TYPO3v4/Documentation/update_txtfiles.sh
+##
+##    #!/bin/bash
+##    # 
+##    # Mass-update of plaintext dumps
+##    # 
+##    # Usage:
+##    #  1. Download DocumentConverter.py from http://www.artofsolving.com/opensource/pyodconverter
+##    #  2. Start OpenOffice.org with the option to listen for incoming UDP requests:
+##    #       oowriter  -accept="socket,port=8100;urp;"
+##    #  3. Run this script (eventually you will need to adjust the path to DocumentConverter.py)
+##    # 
+##    for SOURCE in $(find * -name "*.sxw"); do
+##    	TARGET=$(dirname $SOURCE)/$(basename $SOURCE .sxw).txt
+##    	echo Updating $TARGET ...
+##    	/usr/bin/env python /usr/local/bin/DocumentConverter.py $SOURCE $TARGET
+##    done
+
